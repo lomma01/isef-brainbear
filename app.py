45ece718612e699a21de7f65c5df7d0ff4c2dfda
lomma01 <59766382+lomma01@users.noreply.github.com>
2022-03-01 17:30:57 +0000

    added routes, header and footer

--- app.py
+++ app.py
@@ -12,5 +12,40 @@ def index():
     return render_template('home.html')
 
 
+@app.route('/single')
+def single():
+    return render_template('single.html')
+
+
+@app.route('/multi')
+def multi():
+    return render_template('multi.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/rank')
+def rank():
+    return render_template('rank.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
