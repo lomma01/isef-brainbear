c7b418cc61e4b2f483e9f535c13532968f2d39e0
lomma01 <59766382+lomma01@users.noreply.github.com>
2022-03-03 16:25:57 +0000

    added auth0 provider

--- app.py
+++ app.py
@@ -1,50 +1,141 @@
-from flask import Flask, render_template
+from functools import wraps
+import json
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+import auth
 
 app = Flask(__name__)
 
 # Secret key
 app.config['SECRET_KEY'] = "T5BPYMJD9GVKURSGTAXC"
 
+# Auth0 Provider
+oauth = OAuth(app)
+auth0 = oauth.register(
+    'auth0',
+    client_id=auth.client_id,
+    client_secret=auth.client_secret,
+    api_base_url=auth.api_base_url,
+    access_token_url=auth.access_token_url,
+    authorize_url=auth.authorize_url,
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
+# Decorator
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'profile' not in session:
+            # Redirect to Login page here
+            return redirect('/')
+        return f(*args, **kwargs)
+
+    return decorated
+
 
 # Routes
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
 
 
 @app.route('/single')
+@requires_auth
 def single():
-    return render_template('single.html')
+    return render_template('single.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
 
 
 @app.route('/multi')
+@requires_auth
 def multi():
-    return render_template('multi.html')
+    return render_template('multi.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
 
 
 @app.route('/dashboard')
+@requires_auth
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
 
 
 @app.route('/rank')
+@requires_auth
 def rank():
-    return render_template('rank.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+    return render_template('rank.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
 
 
 @app.route('/about')
 def about():
+    return render_template('about.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'],
+                                                      indent=4))
+
+
+@app.route('/about2')
+def about2():
     return render_template('about.html')
+
+
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=auth.redirect_uri)
+
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {
+        'returnTo': url_for('index', _external=True),
+        'client_id': auth.client_id
+    }
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
 if __name__ == "__main__":
