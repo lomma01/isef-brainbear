fcfb2bf8517697db616b9436ba7bf40135d3153b
xamid1g <59029239+xamid1g@users.noreply.github.com>
2022-03-11 09:23:49 +0100

    added primary key to user_id

--- database.py
+++ database.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully")
 #columns -> you also have to change it in @app.route('/callback')!!!
-conn.execute('CREATE TABLE students (id INTEGER NOT NULL PRIMARY KEY,nickname TEXT NOT NULL, user_id TEXT NOT NULL)')
+conn.execute('CREATE TABLE students (id INTEGER UNIQUE NOT NULL ,user_id TEXT PRIMARY KEY NOT NULL, username TEXT NOT NULL)')
 print ("Table created successfully")
 conn.close()
